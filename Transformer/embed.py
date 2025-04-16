8b4743a37ea14f34538f8551fec1bc551045c6ea
simon skywalker <1470254819@qq.com>
2025-04-16 11:34:00 +0800

    增加了文档并进行补充

--- Transformer/embed.py
+++ Transformer/embed.py
@@ -13,20 +13,22 @@ class Embedder(nn.Module):
         return self.embed(x)
 
 
-'''1.1.1 嵌入表示层'''
 class PositionalEncoder(nn.Module):
+    # d_model：嵌入向量的维度（模型维度）
+    # max_seq_len：最大序列长度，默认为80
+    # dropout：随机丢弃率，用于防止过拟合
     def __init__(self, d_model, max_seq_len=80, dropout=0.1):
         super().__init__()
         self.d_model = d_model
         self.dropout = nn.Dropout(dropout)
-        # 根据pos和i创建一个常量PE矩阵
+        # 根据pos和i创建一个形状为(max_seq_len, d_model)常量PE矩阵 pos：位置索引 i维度索引
         pe = torch.zeros(max_seq_len, d_model)
         for pos in range(max_seq_len):
             for i in range(0, d_model, 2):
                 pe[pos, i] = math.sin(pos / (10000 ** (i / d_model)))
                 if i + 1 < d_model:
                     pe[pos, i + 1] = math.cos(pos / (10000 ** (i / d_model)))
-        
+        # 增加一个批处理维度，将形状从 [max_seq_len, d_model] 变为 [1, max_seq_len, d_model]
         pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
 
